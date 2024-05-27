a43f3efa21f2873a9aa78f8b1b8fa29bb0941230
Luis Reino <luisreinoch@gmail.com>
2024-05-27 13:04:08 -0500

    Creación del flujo 'cheese.flow' para realizar pedidos de productos de queso.

---
+++ src/flows/flowOrders/cheese.flow.js
@@ -0,0 +1,112 @@
+import { addKeyword, EVENTS } from "@builderbot/bot";
+
+import AttemptHandler from "../../functions/AttemptHandler.js";
+import { orderFlow } from "./order.flow.js";
+
+const cheeseFlow = addKeyword(EVENTS.ACTION)
+  .addAnswer(['Selecciona el tipo de queso que desea', '\nPor favor escribe el número de alguna de las opciones:', '\n1️⃣ Semi Duro', '2️⃣ Chicloso', '3️⃣ Suave', '4️⃣ Requesón', '5️⃣ Pasteurizado'],
+    { capture: true }, async (ctx, { state, fallBack, endFlow }) => {
+      // Crear una instancia de AttemptHandler
+      const attemptHandler = new AttemptHandler(state);
+
+      if (ctx.body.toLowerCase() === 'cancelar') {
+        await state.update({ order: [], tries: 0 });
+        return endFlow('Pedido, cancelado con éxito.');
+      }
+
+      // Verificador de respuesta válida y de intentos 
+      if (!["1", "2", "3", "4", "5"].includes(ctx.body)) {
+        // Manejo de Intentos Fallidos
+        const reachedMaxAttempts = await attemptHandler.handleTries();
+        if (reachedMaxAttempts) {
+          await state.update({ order: [], tries: 0 });
+          return endFlow('Has alcanzado el número máximo de intentos. Inténtalo más tarde.');
+        }
+        return fallBack('Respuesta no válida, por favor selecciona una de las opciones.');
+      }
+
+      let productResponse = '';
+
+      switch (ctx.body) {
+        case "1":
+          productResponse = 'Semi Duro';
+          break;
+        case "2":
+          productResponse = 'Chicloso';
+          break;
+        case "3":
+          productResponse = 'Suave';
+          break;
+        case "4":
+          productResponse = 'Requesón';
+          break;
+        case "5":
+          productResponse = 'Pasteurizado';
+          break;
+      }
+
+      await state.update({ product: productResponse, tries: 0 });
+    })
+
+  .addAnswer('Escribe la cantidad de unidades que deseas: ',
+    { capture: true }, async (ctx, { state, fallBack, endFlow }) => {
+      // Crear una instancia de AttemptHandler
+      const attemptHandler = new AttemptHandler(state);
+
+      let order = state.get('order') || [];
+
+      await state.update({ quantity: ctx.body });
+
+      if (ctx.body.toLowerCase() === 'cancelar') {
+        await state.update({ order: [], tries: 0 });
+        return endFlow('Pedido, cancelado con éxito.');
+      }
+
+      const numUnits = /^(100|[1-9][0-9]?$)$/;
+      // Verificador de respuesta válida y de intentos 
+      if (!numUnits.test(ctx.body)) {
+        // Manejo de Intentos Fallidos
+        const reachedMaxAttempts = await attemptHandler.handleTries();
+        if (reachedMaxAttempts) {
+          await state.update({ order: [], tries: 0 });
+          return endFlow('Has alcanzado el número máximo de intentos. Inténtalo más tarde.');
+        }
+        return fallBack('Por favor Escribe una cantidad válida. Solo se pueden hacer pedidos de 1 a 100 unidades.');
+      }
+
+      
+
+      await state.update({ order: order, tries: 0 });
+
+    })
+
+  .addAnswer(['Deseas agregar otro producto?', '\nPor favor escribe *si* o *no*.'], { capture: true }, async (ctx, { state, gotoFlow, fallBack, endFlow }) => {
+    // Crear una instancia de AttemptHandler
+    const attemptHandler = new AttemptHandler(state);
+
+    if (ctx.body.toLowerCase() === 'cancelar') {
+      await state.update({ order: [], tries: 0 });
+      return endFlow('Pedido, cancelado con éxito.');
+    }
+
+    // Verificador de respuesta válida y de intentos 
+    if (!["si", "no"].includes(ctx.body.toLowerCase())) {
+      // Manejo de Intentos Fallidos
+      const reachedMaxAttempts = await attemptHandler.handleTries();
+      if (reachedMaxAttempts) {
+        await state.update({ order: [], tries: 0 });
+        return endFlow('Has alcanzado el número máximo de intentos. Inténtalo más tarde.');
+      }
+      return fallBack('Por favor Escribe una opción válida, solo puedes seleccionar *si* o *no*.');
+    }
+
+    if (ctx.body.toLowerCase() === 'si') {
+      await state.update({ tries: 0 });
+      return gotoFlow(orderFlow);
+    } else if (ctx.body.toLowerCase() === 'no') {
+      await state.update({ tries: 0 });
+      return console.log('Pedido Finalizado');
+    }
+  });
+
+export { cheeseFlow };
